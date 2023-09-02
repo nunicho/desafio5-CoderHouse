6d00e1eac70483fc184b1e001e8d547ca7eaf764
MauricioAlonso <program.alonso@gmail.com>
2023-09-01 23:28:16 -0300

    si se llega al final del listado se reinicia el array

--- src/app.js
+++ src/app.js
@@ -42,14 +42,14 @@ app.get("/", (req, res) => {
   const array = arrayProducts;
   const totalProducts = array.length;
 
-  // Ensure index is within valid range
+   const lastIndex = array.length - 1;
+
   if (index < 0) {
-    index = 0;
+    index = lastIndex;
   } else if (index >= totalProducts) {
-    index = totalProducts - 1;
+    index = 0;
   }
 
-  // Obtiene los datos del producto actual
   const product = array[index];
 
   res.header("Content-type", "text/html");
@@ -66,3 +66,72 @@ console.log(`Server escuchando en puerto ${PORT}`);
 })
 
 
+/*
+const express = require('express')
+const productsRouter = require('./routes/products.router.js');
+const cartsRouter = require('./routes/carts.router.js')
+const arrayProducts = require('./archivos/productos.json')
+
+
+// HANDLEBARS - importación
+const handlebars = require('express-handlebars')
+
+const PORT=8080;
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+app.use('/api/products',productsRouter)
+app.use("/api/carts", cartsRouter);
+
+// HANDLEBARS - inicialización
+
+const hbs = handlebars.create({
+  helpers: {
+    add: function (value, addition) {
+      return value + addition;
+    },
+    subtract: function (value, subtraction) {
+      return value - subtraction;
+    },
+  },
+});
+
+app.engine('handlebars', hbs.engine)
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars')
+
+
+
+app.get("/", (req, res) => {
+  let index = parseInt(req.query.index) || 0;
+  const array = arrayProducts;
+  const totalProducts = array.length;
+
+
+
+  if (index < 0) {
+    index = 0;
+  } else if (index >= totalProducts) {
+    index = totalProducts - 1;
+  }
+
+  const product = array[index];
+
+  res.header("Content-type", "text/html");
+  res.status(200).render("products", {
+    product: product,
+    index: index,
+  });
+});
+
+app.use(express.static(__dirname+'/public'))
+
+const server=app.listen(PORT,()=>{
+console.log(`Server escuchando en puerto ${PORT}`);
+})
+
+*/
